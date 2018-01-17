72f47d080b324c14b7f97dac605651d2146051af
Scott <sstrubberg@us.ibm.com>
2018-01-16 22:02:18 -0600

    spotify start

--- liri.js
+++ liri.js
@@ -1,20 +1,54 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require('fs');
+var inquirer = require('inquirer');
 var nodeSpotifyAPI = require('node-spotify-api');
 var Twitter = require('twitter');
 var request = require('request');
 
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["my-tweets", "spotify-this-song"],
+            name: "options"
+        }
+    ])
+    .then(function(response) {
+        if(response.options === "my-tweets"){
+            myTweets();
+        }
+        if(response.options === "spotify-this-song"){
+            spotifyThisSong();
+        }
+    });
+
+
 var client = new Twitter(keys.twitter);
-var params = {screen_name: 'strubigblue'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for (i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].text);
-    } 
-  }
-});
+
+function myTweets() {
+    var params = {screen_name: 'strubigblue'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+        console.log("Here are " + tweets.truncated + "'s most recent tweets: ");
+        for (i = 0; i < tweets.length; i++) {
+            console.log("- " + tweets[i].text);
+        };
+        };
+    });
+};
 
 
 
-// var spotify = new Spotify(keys.spotify);
+
+var spotify = new nodeSpotifyAPI(keys.spotify);
+
+function spotifyThisSong() {
+    inquirer.prompt([
+        {
+        name: "song",
+        message: "What song would you like to search for?"
+        }
+    ])
+}
